32864e3ab4563d4569ec6f92b0e24e1fbbc91e6b
Kim Vy Vo <kvvo@ucdavis.edu>
2018-08-21 14:39:18 -0700

    stuff

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ var app = express();
 app.use(express.static(__dirname + "/client/dist/client"));
 app.use(bodyParser.json());
 
-require('./server/config/mongoose.js');
-require('./server/config/routes.js')(app);
+// require('./server/config/mongoose.js');
+// require('./server/config/routes.js')(app);
 
 app.all("*", (request ,response ,next) => {
     response.sendFile(path.resolve("./client/dist/client/index.html"))
